8563bb95b332bd1df730f452660c9d69ec54e286
osipm01 <osip2004@vk.com>
2025-11-13 18:48:28 +0300

    add up and down move

--- treng/renderer/Map.ts
+++ treng/renderer/Map.ts
@@ -7,17 +7,20 @@ export class Map {
     maxSpeed: number;
     acceleration: number;
     friction: number;
+    speedY: number; // Добавляем отдельную скорость для вертикального движения
 
     constructor(img: string, vec: Vector2D) {
         this.img = img;
         this.vec = vec;
         this.speed = 0;
+        this.speedY = 0; // Инициализируем вертикальную скорость
         this.maxSpeed = 5;
         this.acceleration = 0.2;
         this.friction = 0.1;
     }
 
     update(ctx: CanvasRenderingContext2D): void {
+        // Применяем трение для горизонтального движения
         if (this.speed > 0) {
             this.speed -= this.friction;
             if (this.speed < 0) this.speed = 0;
@@ -26,8 +29,20 @@ export class Map {
             if (this.speed > 0) this.speed = 0;
         }
         
-        this.vec.x += this.speed;
+        // Применяем трение для вертикального движения
+        if (this.speedY > 0) {
+            this.speedY -= this.friction;
+            if (this.speedY < 0) this.speedY = 0;
+        } else if (this.speedY < 0) {
+            this.speedY += this.friction;
+            if (this.speedY > 0) this.speedY = 0;
+        }
         
+        // Обновляем позицию
+        this.vec.x += this.speed;
+        this.vec.y += this.speedY;
+        
+        // Отрисовываем карту
         ctx.fillStyle = this.img;
         ctx.fillRect(this.vec.x, this.vec.y, 200, 200);
     }
@@ -36,6 +51,27 @@ export class Map {
         this.speed += this.acceleration;
         if (this.speed > this.maxSpeed) {
             this.speed = this.maxSpeed;
+        }
+    }
+
+    moveLeft(): void {
+        this.speed -= this.acceleration;
+        if (this.speed < -this.maxSpeed) {
+            this.speed = -this.maxSpeed;
+        }
+    }
+
+    moveUp(): void {
+        this.speedY -= this.acceleration; // Движение вверх - отрицательная скорость по Y
+        if (this.speedY < -this.maxSpeed) {
+            this.speedY = -this.maxSpeed;
+        }
+    }
+
+    moveDown(): void {
+        this.speedY += this.acceleration; // Движение вниз - положительная скорость по Y
+        if (this.speedY > this.maxSpeed) {
+            this.speedY = this.maxSpeed;
         }
     }
 }
